126bcec20b4b582a58690ecf82a71a5ddd99a754
Dainius <dainius1120@gmail.com>
2025-11-13 15:06:52 +0200

    Update run.bat for optimized build configuration

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
-cmake CMakeLists.txt
-cmake --install .
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="-O3 -funroll-loops -march=native" CMakeLists.txt
 cmake --build .
+cmake --install .
 Work_with_Files.exe
 pause
+
