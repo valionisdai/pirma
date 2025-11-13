b8af2bc2a85191e2590b90172673d48a728d17da
Dainius <dainius1120@gmail.com>
2025-11-13 13:32:47 +0200

    Update mylib.h

--- Include/mylib.h
+++ Include/mylib.h
@@ -154,23 +154,24 @@ Container<Studentas<Container>> failas(int b, string failvardas)
 {
     string failvar;
     Container<Studentas<Container>> Grupe;
-    if("1"==failvardas)
-    {
-        int l;
-        cout << "Is kurio failo norite skaityti duomenis?\n(1) kursiokai.txt\n(2) studentai10000.txt\n(3) studentai100000.txt\n(4) studentai1000000.txt" << endl;
-    while(true)
-    {if(cin >> l && l>=1 && l<=4) break; cout << "Neteisinga ivestis, pasirinkite skaiciu nuo 1 iki 4:" << endl; cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');}
-    if(l==1)
-         failvar = "kursiokai.txt";
-    else if(l==2)
-         failvar = "studentai10000.txt";
-    else if(l==3)
-         failvar = "studentai100000.txt";
-    else if(l==4)
-         failvar = "studentai1000000.txt";
-    }
-    else failvar = failvardas;
-    stringstream buffer = skaitymas(failvar);
+    // if("1"==failvardas)
+    // {
+    //     int l;
+    //     cout << "Is kurio failo norite skaityti duomenis?\n(1) kursiokai.txt\n(2) studentai10000.txt\n(3) studentai100000.txt\n(4) studentai1000000.txt" << endl;
+    // while(true)
+    // {if(cin >> l && l>=1 && l<=4) break; cout << "Neteisinga ivestis, pasirinkite skaiciu nuo 1 iki 4:" << endl; cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');}
+    // if(l==1)
+    //      failvar = "./src/kursiokai.txt";
+    // else if(l==2)
+    //      failvar = "./src/studentai10000.txt";
+    // else if(l==3)
+    //      failvar = "./src/studentai100000.txt";
+    // else if(l==4)
+    //      failvar = "./src/studentai1000000.txt";
+    // }
+    // else 
+    failvar = failvardas;
+    stringstream buffer = skaitymas("./src/"+failvar);
     string line, z;
 
     int k = 0;
@@ -207,7 +208,8 @@ Container<Studentas<Container>> failas(int b, string failvardas)
         Grupe.emplace_back(std::move(laik));
     }
     auto endas = chrono::high_resolution_clock::now();
-    cout << "Rezultatu skaiciavimo laikas: " << chrono::duration_cast<chrono::seconds>(endas - startas).count() << " s" << endl;
+    chrono::duration<double> elapsed = endas - startas;
+    cout << "Rezultatu skaiciavimo laikas: " << fixed << setprecision(3) << elapsed.count() << " s" << endl;
 
     return Grupe;
 }
@@ -229,7 +231,8 @@ void rusiavimas(int b, Container<Studentas<Container>>& Grupe, string failovarda
           default: return false;
          }});
     auto end = chrono::high_resolution_clock::now();
-    cout << "Rikiavimo laikas naudojant konteineri list: " << chrono::duration_cast<chrono::seconds>(end - start).count() << " s\n";
+    chrono::duration<double> elaps = end - start;
+    cout << "Rikiavimo laikas naudojant konteineri list: " << setprecision(3) << elaps.count() << " s\n";
     }
     else
     {
@@ -245,33 +248,54 @@ void rusiavimas(int b, Container<Studentas<Container>>& Grupe, string failovarda
          }
         });
         auto end = chrono::high_resolution_clock::now();
-        cout << "Rikiavimo laikas naudojant konteineri vector: " << chrono::duration_cast<chrono::seconds>(end - start).count() << " s\n";
+        chrono::duration<double> elaps = end - start;
+    cout << "Rikiavimo laikas naudojant konteineri vector: " << fixed << setprecision(3) << elaps.count() << " s\n";
     }
-    auto startas = chrono::high_resolution_clock::now();
-    Container<Studentas<Container>> gerai, blogai;
-    for(const auto& temp : Grupe)
+    Container<Studentas<Container>> blogai;
+    if constexpr (std::is_same_v<std::remove_cv_t<std::remove_reference_t<decltype(Grupe)>>,std::list<Studentas<Container>>>)
     {
-        if(temp.rez>=5)
-            gerai.push_back(temp);
-        else
-            blogai.push_back(temp);
+        auto startas = chrono::high_resolution_clock::now();
+
+        for(auto it=Grupe.begin(); it!=Grupe.end();)
+        {
+            if(it->rez >= 5)
+                blogai.splice(blogai.end(), Grupe, it++);
+            else ++it;
+        }
+
+        auto endas = chrono::high_resolution_clock::now();
+        chrono::duration<double> elapsed = endas - startas;
+        cout << "Rusiavimo laikas naudojant list: " << fixed << setprecision(3) << elapsed.count() << " s" << endl;
     }
-    auto endas = chrono::high_resolution_clock::now();
-    cout << "Rusiavimo laikas: " << chrono::duration_cast<chrono::seconds>(endas - startas).count() << " s" << endl;
-    isvedimas(b, gerai, "gerai_"+failovardas);
+    else
+    {
+        auto startas = chrono::high_resolution_clock::now();
+
+
+        auto it = partition(Grupe.begin(), Grupe.end(), [](const auto& s) {return s.rez < 5;});
+        blogai.insert(blogai.end(), std::make_move_iterator(it), std::make_move_iterator(Grupe.end()));
+        Grupe.erase(it, Grupe.end());
+
+
+        auto endas = chrono::high_resolution_clock::now();
+        chrono::duration<double> elapsed = endas - startas;
+        cout << "Rusiavimo laikas naudojant vector: " << fixed << setprecision(3) << elapsed.count() << " s" << endl;
+    }
+
+    isvedimas(b, Grupe, "gerai_"+failovardas);
     isvedimas(b, blogai, "blogai_"+failovardas);
 }
 
 template<template<typename> class Container>
 void isvedimas(int b, const Container<Studentas<Container>>& Grupe, string failovardas)
 {
-    auto startas = chrono::high_resolution_clock::now();
-    if("1"==failovardas)
-        failovardas = "rez.txt";
-    else
+    if(failovardas.find("txt") != std::string::npos)
         failovardas = "rez_"+failovardas;
+    else
+        failovardas = "rez_"+failovardas +".txt";
     ofstream wr(failovardas);
     ostringstream oss;
+    auto startas = chrono::high_resolution_clock::now();
     oss << setw(21) << right << "Vardas | " << setw(21) << right << "Pavarde | " << setw(21) << right;
     if(b==1) {oss << "Galutinis (Vid.)"  << " | Adresas atmintyje" << " \n----------------------------------------------------------------" << endl;}
             else if(b==2) {oss << "Galutinis (Med.) "  << " | Adresas atmintyje" << "\n-------------------------------------------------------" << endl;}
@@ -287,7 +311,8 @@ void isvedimas(int b, const Container<Studentas<Container>>& Grupe, string failo
     wr << oss.str();
     wr.close();
     auto endas = chrono::high_resolution_clock::now();
-    cout << "Failo " << failovardas << " rasymo laikas: " << chrono::duration_cast<chrono::seconds>(endas - startas).count() << " s" << endl;
+    chrono::duration<double> elapsed = endas - startas;
+    cout << "Failo " << failovardas << " rasymo laikas: " << fixed << setprecision(3) << elapsed.count() << " s" << endl;
 }
 
 template<template<typename> class Container>
